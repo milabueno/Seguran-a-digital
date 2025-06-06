76553e9bba6836e899e3a6176e39e30735d16827
milabueno <143520429+milabueno@users.noreply.github.com>
2025-06-06 11:46:24 -0300

    de

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
-const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
-const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz';
-const numeros = '0123456789';
-const simbolos = '!@%*?';
+const letrasMaiusculas = 'ACBEDGFIHKJMLONQPSRUTWVXZY';
+const letrasMinusculas = 'acbedgfihkjmlonqpsrutwvxzy';
+const numeros = '0214365879';
+const simbolos = '§@#&*';
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 const campoSenha = document.querySelector('#campo-senha');
 const checkbox = document.querySelectorAll('.checkbox');
